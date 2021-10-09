68d08e7ca4a9ca26da863c71687f204f8fae1969
Bruce Yang <winwinbruce@gmail.com>
2021-10-09 09:15:26 +0800

    using template in web page

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 # @app.route('/')
 # def index():
 #     user_agent = request.headers.get('User_Agent')
@@ -10,12 +11,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
