6ca74e7e0cfd553d874558522c50de346cfda995
Bruce Yang <winwinbruce@gmail.com>
2021-10-10 15:08:47 +0800

    wtf form for web form

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very hard to guess string'
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('Whats yours name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
 
 @app.errorhandler(404)
@@ -16,9 +29,17 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',
+                           form=form,
+                           name=name,
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
