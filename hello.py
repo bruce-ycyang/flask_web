21c60b62e03707c8d3bf30dd2a056edb9a158b19
Bruce Yang <winwinbruce@gmail.com>
2021-10-10 08:48:52 +0800

    add error page

--- hello.py
+++ hello.py
@@ -2,11 +2,18 @@ from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
-# @app.route('/')
-# def index():
-#     user_agent = request.headers.get('User_Agent')
-#     return '<h1>Hello, {}</h1>'.format(user_agent)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
